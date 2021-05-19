0ab1d6fe4800db6d6be53a82b6fb8960aedca469
husdev <hus.bs4dev@gmail.com>
2021-05-20 00:13:23 +0300

    Add testimonial card component

---
+++ src/components/TestimonialCard.svelte
@@ -0,0 +1,72 @@
+<div class="testimonial-card">
+    <div class="profile-info">
+        <div>
+            <img class="img-profile" src="assets/img/Polina_Nazarova-min.png" alt="">
+            <div class="description">
+                <span class="name">Irene Pereyra</span>
+                <span>Interaction Design Fellow ‘19</span>
+            </div>
+        </div>
+        <img src="assets/img/linked-in.svg" alt="">
+    </div>
+
+    <div class="paragraph">
+        <p>
+            This Fellowship was a turning point in my career. I wouldn’t be where I am today without the financial support and experienced offered through the program. 
+        </p>
+        <span>Education · B.A. Visual Design</span> 
+    </div>
+</div>
+
+<style>
+    .testimonial-card { 
+        border: 1px solid var(--gray-3);
+        color: #6A6A6A;
+    }
+
+    .paragraph { 
+        background-color: var(--gray-5);
+        padding: 40px 100px;
+        font-size: larger;
+        font-weight: 300;
+    }
+
+    .paragraph span {
+        font-size: small;
+    }
+
+    .profile-info { 
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .profile-info { 
+        padding: 20px;
+    }
+
+    .profile-info div {
+        display: flex;
+        align-items: center;
+    }
+
+    .profile-info div .description { 
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 20px;
+    }
+
+    .name { 
+        color: var(--gray-1);
+        font-weight: 500;
+    }
+
+    img.img-profile { 
+        width: 100px;
+        height: 100px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+
+</style>
