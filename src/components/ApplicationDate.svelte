53b07c97dc2858fff238296f1b15f3a6d5ed241e
husdev <hus.bs4dev@gmail.com>
2021-05-20 00:13:59 +0300

    Add design application close date component

---
+++ src/components/ApplicationDate.svelte
@@ -0,0 +1,25 @@
+<div class="application-close">
+    <p>Application closes in</p>
+    <span>6 Day : 22 Hrs : 56 Min : 13 Seg</span>
+</div>
+
+<style>
+    .application-close { 
+        padding: 20px;
+        border-radius: 4px;
+        border: var(--gray-3) solid 1px;
+    }
+
+    .application-close p { 
+        color: var(--purple);
+        font-weight: 500;
+    }
+
+    .application-close span { 
+        color: var(--gray-1);
+        font-size: x-large;
+        font-weight: 300;
+    }
+
+
+</style>
