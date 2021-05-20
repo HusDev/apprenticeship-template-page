bf889ff4f94e79d4c8fe4ea72e68bbe6aad4d06d
husdev <hus.bs4dev@gmail.com>
2021-05-20 03:13:05 +0300

    Add toggle button component

--- src/components/MainButton.svelte
+++ src/components/MainButton.svelte
@@ -1,19 +1,17 @@
-<button>
-    Apply Now
-</button>
+<button> Apply Now </button>
 
 <style>
-    button { 
-        background-color: var(--purple);
-        padding: 15px 40px;
-        border: 0px;
-        border-radius: 29px;
-        color: white;
-        font-weight: 500;
-    }
+  button {
+    background-color: var(--purple);
+    padding: 15px 40px;
+    border: 0px;
+    border-radius: 29px;
+    color: white;
+    font-weight: 500;
+  }
 
-    button:hover {
-        background-color: #5a51ac;
-        cursor: pointer;
-    }
+  button:hover {
+    background-color: #5a51ac;
+    cursor: pointer;
+  }
 </style>
---
+++ src/components/ReadMoreButton.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Fa from "svelte-fa/src/fa.svelte";
+  import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
+  export let isMore = false;
+  function clickHandle() {
+    isMore = !isMore;
+  }
+</script>
+
+<div class="read-more {isMore ? 'more' : ''}" on:click={clickHandle}>
+  {#if isMore}
+    <Fa icon={faMinus} />
+  {:else}
+    <Fa icon={faPlus} />
+  {/if}
+</div>
+
+<style>
+  .read-more {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #959595;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    border: #959595 solid 1px;
+    cursor: pointer;
+  }
+
+  .more {
+    background-color: var(--purple);
+    color: white;
+    border: var(--purple) solid 1px;
+  }
+</style>
