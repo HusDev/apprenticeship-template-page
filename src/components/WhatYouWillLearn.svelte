dc3fe0f6f36a84e5bd8a2f152b89462a42d34e0a
husdev <hus.bs4dev@gmail.com>
2021-05-20 05:03:22 +0300

    Add what you will learn component

---
+++ src/components/WhatYouWillLearn.svelte
@@ -0,0 +1,55 @@
+<script>
+  import ReadMoreButton from "./ReadMoreButton.svelte";
+  let isMore = false;
+</script>
+
+<div class="fq">
+  <div class="heading">
+    <div>
+      <span class="title">Do I get a job placement upon graduation?</span>
+    </div>
+    <div class="toggle-btn">
+      <ReadMoreButton bind:isMore />
+    </div>
+    <p class={isMore ? "" : "hidden"}>
+      The majority of our students receive numerous job offers at the end of the
+      second academic year of their Bachelor's programme and at the end of the
+      first academic year of their Master's programme. The best applicants
+      receive an offer from our industrial partners at the beginning of their
+    </p>
+  </div>
+</div>
+
+<style>
+  .fq {
+    border-bottom: solid 1px var(--gray-3);
+    padding: 20px;
+  }
+
+  .heading {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  p {
+    grid-column: 1/1;
+    grid-row: 2/2;
+    display: block;
+  }
+
+  .title {
+    font-weight: 500;
+    font-size: large;
+    color: var(--gray-1);
+  }
+
+  .hidden {
+    display: none;
+  }
+
+  .toggle-btn { 
+    justify-self: end;
+  }
+</style>
