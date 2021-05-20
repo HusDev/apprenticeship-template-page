2ae63c712a3d782f81b22a3d0b5cafdc6887d9d4
husdev <hus.bs4dev@gmail.com>
2021-05-20 03:14:08 +0300

    Add commitment component

---
+++ src/components/Commitment.svelte
@@ -0,0 +1,43 @@
+<div class="commitment-card">
+  <span class="title">Study commitment</span>
+  <span class="effort">3 hours / day</span>
+  <div class="line" />
+  <p>
+    You will complete 15 modules to graduate. Daily classes are 3 hours, plus
+    coursework to complete in your own time.
+  </p>
+</div>
+
+<style>
+  .commitment-card {
+    display: flex;
+    flex-direction: column;
+    padding: 30px 20px;
+    border: var(--gray-3) solid 1px;
+    border-radius: 4px;
+  }
+
+  .title {
+    color: var(--purple);
+    margin-bottom: 10px;
+    font-weight: 500;
+    font-size: medium;
+  }
+
+  .effort {
+    font-size: x-large;
+    font-weight: 300;
+    color: var(--gray-2);
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-weight: 300;
+    color: var(--gray-2);
+  }
+
+  .line {
+    border-bottom: solid 1px var(--gray-3);
+    width: 20px;
+  }
+</style>
