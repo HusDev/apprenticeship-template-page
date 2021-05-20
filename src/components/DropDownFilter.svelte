cc0bcc3896b7b0350aa41ccec9cf6803dba74fa2
husdev <hus.bs4dev@gmail.com>
2021-05-20 05:01:25 +0300

    Add dropdown component

---
+++ src/components/DropDownFilter.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Fa from "svelte-fa/src/fa.svelte";
+  import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
+  export let isExpand = false;
+  function clickHandle() {
+    isExpand = !isExpand;
+  }
+</script>
+
+<div on:click={clickHandle} class="dropdown {isExpand ? 'expand' : ''}">
+  <span>All</span>
+  {#if isExpand}
+    <Fa icon={faAngleUp} />
+  {:else}
+    <Fa icon={faAngleDown} />
+  {/if}
+  <div class="dropdown-content {isExpand ? 'active' : ''}">
+    <p>Hello World!</p>
+    <p>Hello World!</p>
+    <p>Hello World!</p>
+    <p>Hello World!</p>
+  </div>
+</div>
+
+<style>
+  .dropdown {
+    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    width: 200px;
+    padding: 15px;
+    border: var(--gray-3) solid 1px;
+    border-radius: 30px;
+    color: var(--purple);
+    cursor: pointer;
+    z-index: 1;
+    background-color: white;
+  }
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    min-width: 160px;
+    padding-top: 10px;
+    z-index: 2;
+  }
+
+  .active {
+    display: block;
+  }
+
+  .expand {
+    height: 150px;
+    border-radius: 25px;
+  }
+</style>
